af54d588ae68c6e0fbfecb4d26ef70476d533724
dlworldpeace <xdl_line@hotmail.com>
2019-01-20 17:40:04 +0800

    MainActivity: Add Navigation Drawer template.

--- loadingActivity/src/main/java/com/example/livenessproject/MainActivity.kt
+++ loadingActivity/src/main/java/com/example/livenessproject/MainActivity.kt
@@ -1,10 +1,16 @@
 package com.example.livenessproject
 
-import android.support.v7.app.AppCompatActivity
+import android.content.Context
+import android.graphics.Color
 import android.os.Bundle
 import android.os.Handler
+import android.support.v4.view.GravityCompat
+import android.support.v7.app.ActionBarDrawerToggle
+import android.support.v7.app.AppCompatActivity
+import android.view.View
 import android.widget.Toast
 import com.megvii.livenessproject.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,6 +19,59 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        init()
+        val ss:String = intent.getStringExtra("json")
+        text_view.setText(ss)
+    }
+
+    private fun init() {
+        // Configure action bar
+        setSupportActionBar(toolbar)
+        val actionBar = supportActionBar
+        actionBar?.title = getString(R.string.app_name)
+
+        // Initialize the action bar drawer toggle instance
+        val drawerToggle: ActionBarDrawerToggle = object : ActionBarDrawerToggle(
+                this,
+                drawer_layout,
+                toolbar,
+                R.string.drawer_open,
+                R.string.drawer_close
+        ){
+            override fun onDrawerClosed(view: View){
+                super.onDrawerClosed(view)
+                //toast("Drawer closed")
+            }
+
+            override fun onDrawerOpened(drawerView: View){
+                super.onDrawerOpened(drawerView)
+                //toast("Drawer opened")
+            }
+        }
+
+        // Configure the drawer layout to add listener and show icon on toolbar
+        drawerToggle.isDrawerIndicatorEnabled = true
+        drawer_layout.setDrawerListener(drawerToggle)
+        drawerToggle.syncState()
+
+
+        // Set navigation view navigation item selected listener
+        navigation_view.setNavigationItemSelectedListener{
+            when (it.itemId){
+                R.id.action_cut -> toast("Cut clicked")
+                R.id.action_copy -> toast("Copy clicked")
+                R.id.action_paste -> toast("Paste clicked")
+                R.id.action_new ->{
+                    // Multiline action
+                    toast("New clicked")
+                    drawer_layout.setBackgroundColor(Color.RED)
+                }
+
+            }
+            // Close the drawer
+            drawer_layout.closeDrawer(GravityCompat.START)
+            true
+        }
     }
 
     override fun onBackPressed() {
@@ -21,8 +80,13 @@ class MainActivity : AppCompatActivity() {
         }
 
         this.doubleBackToExitPressedOnce = true
-        Toast.makeText(this, "Press BACK again to exit", Toast.LENGTH_SHORT).show()
+        toast("Press BACK again to exit")
 
         Handler().postDelayed(Runnable { doubleBackToExitPressedOnce = false }, 2000)
+    }
+
+    // Extension function to show toast message easily
+    private fun Context.toast(message:String){
+        Toast.makeText(applicationContext,message,Toast.LENGTH_SHORT).show()
     }
 }
