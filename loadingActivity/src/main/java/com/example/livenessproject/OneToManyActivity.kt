c04a99307c9d76fdaad0f19604cb1645b79c05d4
dlworldpeace <xdl_line@hotmail.com>
2019-01-20 23:28:37 +0800

    OneToManyActivity: Upload image to image_view either by camera or from gallery

--- loadingActivity/src/main/java/com/example/livenessproject/OneToManyActivity.kt
+++ loadingActivity/src/main/java/com/example/livenessproject/OneToManyActivity.kt
@@ -1,13 +1,90 @@
 package com.example.livenessproject
 
+import android.app.Activity
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.megvii.livenessproject.R
+import android.provider.MediaStore
+import android.support.v7.app.AlertDialog
+import android.graphics.Bitmap
+import android.net.Uri
+import android.os.Environment
+import kotlinx.android.synthetic.main.activity_one_to_many.*
+import java.io.File
+import u.aly.i
+import android.os.Environment.DIRECTORY_DCIM
+import android.os.Environment.getExternalStoragePublicDirectory
+import android.os.StrictMode
+import android.support.v4.media.MediaDescriptionCompatApi21.Builder.build
+import android.os.StrictMode.setVmPolicy
+
+
+
+
+
 
 class OneToManyActivity : AppCompatActivity() {
+
+    private var CONTENT_REQUEST: Int = 1337
+    private var SELECT_FILE:Int = 0
+    private var cameraOutput: File? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_one_to_many)
+
+        fab.setOnClickListener { selectImage() }
+    }
+
+    private fun selectImage() {
+
+        val items = arrayOf<CharSequence>("Take using Camera", "Import from Gallery", "Cancel")
+
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Upload Image")
+
+        builder.setItems(items) { dialogInterface, i ->
+            when(items[i]) {
+                "Take using Camera" -> {
+                    // Prevent FileUriExposedException
+                    val builder = StrictMode.VmPolicy.Builder()
+                    StrictMode.setVmPolicy(builder.build())
+
+                    val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+                    val dir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM)
+                    cameraOutput = File(dir, "CameraContent.jpeg")
+                    intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(cameraOutput))
+                    startActivityForResult(intent, CONTENT_REQUEST)
+                }
+                "Import from Gallery" -> {
+                    val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                    intent.type = "image/*"
+                    startActivityForResult(intent, SELECT_FILE)
+                }
+                "Cancel" -> dialogInterface.dismiss()
+            }
+        }
+        builder.show()
+
+    }
+
+    public override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+
+        if (resultCode == Activity.RESULT_OK) {
+
+            if (requestCode == CONTENT_REQUEST) {
+
+                image_holder.setImageURI(Uri.fromFile(cameraOutput))
+
+            } else if (requestCode == SELECT_FILE) {
+
+                super.onActivityResult(requestCode, resultCode, data)
+                val selectedImageUri = data!!.data
+                image_holder.setImageURI(selectedImageUri)
+
+            }
+
+        }
     }
 }
