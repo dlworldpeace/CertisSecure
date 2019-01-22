9cfaf80c83c4e3b2c98a9de2267391b8b8b83347
dlworldpeace <xdl_line@hotmail.com>
2019-01-23 01:04:42 +0800

    HttpHelper: allow "GET" request to be sent using the same wrapper. Add listAllUsers request function.

--- loadingActivity/src/main/java/com/example/livenessproject/util/HttpHelper.kt
+++ loadingActivity/src/main/java/com/example/livenessproject/util/HttpHelper.kt
@@ -11,48 +11,52 @@ class HttpHelper {
     companion object {
         fun loginWithPassword(username: String, password: String): String {
             val json = "{\"username\":\"$username\", \"password\":\"$password\"}"
-            return postRequestWrapper("http://3.0.121.132:3000/auth/login", json)
+            return httpRequestWrapper("http://3.0.121.132:3000/auth/login", json)
         }
 
         fun oneToOneComparison(img1base64: String, img2base64: String): String {
             val json = "{\"img1\":\"data:image/jpeg;base64,$img1base64\", \"img2\":\"data:image/jpeg;base64,$img2base64\"}"
-            return postRequestWrapper("http://3.0.121.132:3000/test/compare", json)
+            return httpRequestWrapper("http://3.0.121.132:3000/test/compare", json)
         }
 
         fun oneToManyComparison(imgbase64: String): String {
             val json = "{\"img\":\"data:image/jpeg;base64,$imgbase64\"}"
-            return postRequestWrapper("http://3.0.121.132:3000/auth/loginface", json)
+            return httpRequestWrapper("http://3.0.121.132:3000/auth/loginface", json)
         }
 
-        private fun postRequestWrapper(url: String, json: String): String{
-            // HttpClient 6.0 is already outdated
+        fun listAllUsers(): String {
+            return httpRequestWrapper("http://3.0.121.132:3000/users", null)
+        }
+
+        private fun httpRequestWrapper(url: String, json: String?): String{
             var result = ""
             var reader: BufferedReader? = null
 
             try {
-                val url = URL(url)
-                val conn = url.openConnection() as HttpURLConnection
-                conn.requestMethod = "POST"
-                conn.doOutput = true
-                conn.doInput = true
-                conn.useCaches = false
-                conn.setRequestProperty("Connection", "Keep-Alive")
-                conn.setRequestProperty("Charset", "UTF-8")
-                conn.setRequestProperty("Content-Type", "application/json")
-                // Set accept type here, if not will receive Error 415
-                // You can use conn.setRequestProperty("accept","*/*") to accept all types
-                conn.setRequestProperty("accept", "application/json")
+                val conn = URL(url).openConnection() as HttpURLConnection
+                conn.requestMethod = "GET"
 
-                val writebytes = json.toByteArray()
-                conn.setRequestProperty("Content-Length", writebytes.size.toString())
-                val outwritestream = conn.outputStream
-                outwritestream.write(json.toByteArray())
-                outwritestream.flush()
-                outwritestream.close()
+                if (null != json) {
+                    conn.requestMethod = "POST"
+                    conn.doOutput = true
+                    conn.doInput = true
+                    conn.useCaches = false
+                    conn.setRequestProperty("Connection", "Keep-Alive")
+                    conn.setRequestProperty("Charset", "UTF-8")
+                    conn.setRequestProperty("Content-Type", "application/json")
+                    // Set accept type here, if not will receive Error 415
+                    // You can use conn.setRequestProperty("accept","*/*") to accept all types
+                    conn.setRequestProperty("accept", "application/json")
+                    val writeBytes = json.toByteArray()
+                    conn.setRequestProperty("Content-Length", writeBytes.size.toString())
+                    val outWriteStream = conn.outputStream
+                    outWriteStream.write(json.toByteArray())
+                    outWriteStream.flush()
+                    outWriteStream.close()
+                }
 
                 if (conn.responseCode == 200) {
-                    reader = BufferedReader(
-                            InputStreamReader(conn.inputStream))
+                    reader = BufferedReader(InputStreamReader(conn.inputStream))
                     result = reader.readLine()
                 }
             } catch (e: Exception) {
@@ -69,5 +73,4 @@ class HttpHelper {
             return result
         }
     }
-
 }
