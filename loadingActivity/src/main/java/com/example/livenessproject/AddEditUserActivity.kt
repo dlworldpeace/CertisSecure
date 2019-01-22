121534b924e8aa1d518046e880ae9a88a76fb511
dlworldpeace <xdl_line@hotmail.com>
2019-01-23 01:45:19 +0800

    Add AddEditUserActivity as a page that can add or update a user info to server.

---
+++ loadingActivity/src/main/java/com/example/livenessproject/AddEditUserActivity.kt
@@ -0,0 +1,13 @@
+package com.example.livenessproject
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.megvii.livenessproject.R
+
+class AddEditUserActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_edit_user)
+    }
+}
