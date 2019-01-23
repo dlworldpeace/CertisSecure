8e69fcdbc072eceebb174d5d625ee80360274949
dlworldpeace <xdl_line@hotmail.com>
2019-01-24 01:51:52 +0800

    ImageHelper: Ensure all images uploaded will be of width between 650px and 1300px with its aspect ratio unchanged.

--- loadingActivity/src/main/java/com/example/livenessproject/util/ImageHelper.kt
+++ loadingActivity/src/main/java/com/example/livenessproject/util/ImageHelper.kt
@@ -9,9 +9,11 @@ import java.io.ByteArrayOutputStream
 class ImageHelper {
     companion object {
         fun imageViewToBase64(imageView: ImageView): String {
-            val bitmap = (imageView.drawable as BitmapDrawable).bitmap
+            var bitmap = (imageView.drawable as BitmapDrawable).bitmap
+            while(bitmap.width > 1300) {
+                bitmap = Bitmap.createScaledBitmap(bitmap, bitmap.width / 2, bitmap.height / 2, false)
+            }
             val stream = ByteArrayOutputStream()
-            bitmap.compress(Bitmap.CompressFormat.JPEG, 20, stream)
             val byteArray = stream.toByteArray()
             return Base64.encodeToString(byteArray, Base64.NO_WRAP) //NO_WRAP because the string will be used in json, which does not recognize \n
         }
