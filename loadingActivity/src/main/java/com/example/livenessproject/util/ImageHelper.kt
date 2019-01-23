6c4f816f1d2a99107529ac9b1cfed7cb7103c1b1
dlworldpeace <xdl_line@hotmail.com>
2019-01-24 01:57:41 +0800

    ImageHelper: Add missing line, deleted by mistake.

--- loadingActivity/src/main/java/com/example/livenessproject/util/ImageHelper.kt
+++ loadingActivity/src/main/java/com/example/livenessproject/util/ImageHelper.kt
@@ -14,6 +14,7 @@ class ImageHelper {
                 bitmap = Bitmap.createScaledBitmap(bitmap, bitmap.width / 2, bitmap.height / 2, false)
             }
             val stream = ByteArrayOutputStream()
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 20, stream)
             val byteArray = stream.toByteArray()
             return Base64.encodeToString(byteArray, Base64.NO_WRAP) //NO_WRAP because the string will be used in json, which does not recognize \n
         }
