6ef08d944ca1aa08f61e5bb71cc461457730972f
dlworldpeace <xdl_line@hotmail.com>
2019-01-27 01:34:17 +0800

    ImageHelper: Add a function to convert base64 strings into bitmap format.

--- loadingActivity/src/main/java/com/example/livenessproject/util/ImageHelper.kt
+++ loadingActivity/src/main/java/com/example/livenessproject/util/ImageHelper.kt
@@ -1,6 +1,7 @@
 package com.example.livenessproject.util
 
 import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.graphics.drawable.BitmapDrawable
 import android.util.Base64
 import android.widget.ImageView
@@ -17,6 +18,12 @@ class ImageHelper {
             bitmap.compress(Bitmap.CompressFormat.JPEG, 20, stream)
             val byteArray = stream.toByteArray()
             return Base64.encodeToString(byteArray, Base64.NO_WRAP) //NO_WRAP because the string will be used in json, which does not recognize \n
+        }
+
+        fun ExtendedBase64ToBitmap(extendedBase64: String): Bitmap {
+            val photoData = extendedBase64.substring(extendedBase64.indexOf(",") + 1)
+            val decodedString = Base64.decode(photoData, Base64.DEFAULT)
+            return BitmapFactory.decodeByteArray(decodedString, 0, decodedString.size)
         }
     }
 }
