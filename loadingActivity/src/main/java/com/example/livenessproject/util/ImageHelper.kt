b536df8b1ffc2735a1d8e5451f0270b8d37bac51
dlworldpeace <xdl_line@hotmail.com>
2019-01-27 15:32:09 +0800

    ImageHelper: crop user's image to square at center before converting to base64 string.

--- loadingActivity/src/main/java/com/example/livenessproject/util/ImageHelper.kt
+++ loadingActivity/src/main/java/com/example/livenessproject/util/ImageHelper.kt
@@ -8,13 +8,24 @@ import java.io.ByteArrayOutputStream
 
 class ImageHelper {
     companion object {
-        fun imageViewToBase64(imageView: ImageView): String {
-            var bitmap = (imageView.drawable as BitmapDrawable).bitmap
-            while(bitmap.width > 1300) {
-                bitmap = Bitmap.createScaledBitmap(bitmap, bitmap.width / 2, bitmap.height / 2, false)
+        fun imageViewToRoundBase64(imageView: ImageView): String {
+            val srcBmp = (imageView.drawable as BitmapDrawable).bitmap
+            var dstBmp :Bitmap?
+
+            // Crop to square shape
+            dstBmp = if (srcBmp.width >= srcBmp.height){
+                Bitmap.createBitmap(srcBmp, srcBmp.width/2 - srcBmp.height/2, 0, srcBmp.height, srcBmp.height)
+            }else{
+                Bitmap.createBitmap(srcBmp, 0, srcBmp.height/2 - srcBmp.width/2, srcBmp.width, srcBmp.width)
             }
+
+            // Downsize to range (650px, 1300px)
+            while(dstBmp!!.width > 1300) {
+                dstBmp = Bitmap.createScaledBitmap(dstBmp, dstBmp.width / 2, dstBmp.height / 2, false)
+            }
+
             val stream = ByteArrayOutputStream()
-            bitmap.compress(Bitmap.CompressFormat.JPEG, 20, stream)
+            dstBmp.compress(Bitmap.CompressFormat.JPEG, 20, stream)
             val byteArray = stream.toByteArray()
             return Base64.encodeToString(byteArray, Base64.NO_WRAP) //NO_WRAP because the string will be used in json, which does not recognize \n
         }
@@ -28,14 +39,12 @@ class ImageHelper {
         fun getCroppedCircleBitmap(bitmap: Bitmap): Bitmap {
             val output = Bitmap.createBitmap(bitmap.width, bitmap.height, Bitmap.Config.ARGB_8888)
             val canvas = Canvas(output)
-
-            val color = 0xff424242
             val paint = Paint()
             val rect = Rect(0, 0, bitmap.width, bitmap.height)
 
             paint.isAntiAlias = true
             canvas.drawARGB(0, 0, 0, 0)
-            paint.color = color.toInt()
+            paint.color = 0xff424242.toInt()
             canvas.drawCircle(bitmap.width / 2.toFloat(), bitmap.height / 2.toFloat(),
                     bitmap.width / 2.toFloat(), paint)
             paint.xfermode = PorterDuffXfermode(PorterDuff.Mode.SRC_IN)
